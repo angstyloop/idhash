3f11f78d2e2ccc9d0fd98b75e233cfae6f914649
angstyloop <angstyloop@gmail.com>
2021-08-01 17:40:42 -0700

    enough test coverage now to get off the ground. can make more exhaustive later, but feeling good about it. next steps are header files, and visual testing.

--- test_histogram.c
+++ test_histogram.c
@@ -14,7 +14,7 @@
 
 #include "histogram.c"
 
-void test_histogram_insert(){
+static void test_histogram_insert(){
   guint8 bin_index=0;
   int bit_index=1;
   histogram h={0}; 
@@ -22,7 +22,7 @@ void test_histogram_insert(){
   assert(1==bit_array_get(h.bins[bin_index], bit_index));
 }
 
-void test_histogram_remove(){
+static void test_histogram_remove(){
   guint8 bin_index=0;
   int bit_index=1;
   histogram h={0};
@@ -31,7 +31,7 @@ void test_histogram_remove(){
   assert(0==bit_array_get(h.bins[bin_index], bit_index));
 }
 
-void test_histogram_get(){
+static void test_histogram_get(){
   guint8 bin_index=0;
   int bit_index=1;
   histogram h={0};
@@ -80,63 +80,24 @@ histogram* init_test_histogram_1(histogram* h){
 }
 
 /* 
- * Set every bin in the histogram to 2^64-1.
- *
- * So the histogram of bins will look like this
- *
- * {2^64-1, 2^64-1, ..., 2^64-1} (256 entries).
- *  ^
- *  |
- *  .__ A single bin, represented by a 64-bit integer.
- *
- * The union will look like this:
- *
- * 2^64 - 1 = G_MAXUINT64 =
- *
- * 1111111111111111111111111111111111111111111111111111111111111111 
- *
- * But the importance will be 2^64 - 1 - 2^32 - 1 = G_MAXUINT64 =
- *
- * 1111111111111111111111111111111111111111111111111111111111111111
- * |                              ||                              |
- * .______________________________..______________________________.
- *             32 bits                          32 bits
- *
- * since they lie below the median, which is simply the middle, since every
- * bin has a single bit set to 1.
- *
- */
-histogram* init_test_histogram_2(histogram* h){
-  for(int i=0; i<256; ++i){
-    h->bins[i] = G_MAXUINT64; // all 1's
-  }
-  h->median = 128; // Don't worry, test_histogram_median will override this.
-  return h;        //   It's just there for test_histogram_importance.
-}
-
-/* 
  * Test histogram_median(histogram*).
  * 
  * Use the test histograms initialized by init_test_histogram_{1..3}() to 
  * test the histogram_median function. 
  *
  */
-void test_histogram_median(){
+static void test_histogram_median(){
   histogram h={0};
   histogram_median(init_test_histogram_1(&h));
-
   assert(h.median == 128);
   memset(&h, 0, sizeof(h));
 
-  histogram_median(init_test_histogram_2(&h));
-  assert(h.median == 128);
-
-  memset(&h, 0, sizeof(h));
-  const bin_index = 0;
-  const bit_index = 0;
+  /* Put all 64 in the first bin. Then the median is just the first bin. 
+   */
+  const guint8 bin_index = 0;
   h.bins[bin_index] = G_MAXINT64;
   histogram_median(&h);
-  assert(h.median = 0);
+  assert(h.median == 0);
 }
 
 
@@ -147,17 +108,10 @@ void test_histogram_median(){
  * test the histogram_importance function. 
  *
  */
-void test_histogram_importance(){
+static void test_histogram_importance(){
   histogram h={0};
   histogram_importance(init_test_histogram_1(&h));
-
   assert(h.importance == G_MAXUINT64 - G_MAXUINT32);
-  memset(&h, 0, sizeof(h));
-
-  histogram_importance(init_test_histogram_2(&h));
-  assert(h.importance == G_MAXUINT64);
-
-  assert(h.importance == G_MAXUINT64);
 }
 
 
@@ -180,44 +134,50 @@ PixelRGB* init_test_pixels_1(PixelRGB pixels [64]){
   return pixels;
 }
 
-void test_histogram_process_pixel_pair(){
-  const int bin_index=1-0;
-  const int index=0;
-  const int next=1;
+static void test_histogram_process_pixel_pair(){
   histogram h={0};
   PixelRGB pixels[64]={0};
 
-  /* Neigboring pixels have difference 0
+  /* Process the first (horizontal) pair of pixels created by 
+   * init_test_pixels_0.
    */
-  histogram_process_pixel_pair(&h, init_test_pixels_0(pixels), index, next);
-  assert(histogram_get(&h, bin_index, index) == 0);
+  histogram_process_pixel_pair(&h, init_test_pixels_0(pixels), 0, 1);
+  /* Neigboring pixels have difference 0.
+   */
+  assert(histogram_get(&h, 0, 0) == 1);
 
   memset(pixels, 0, 64*(sizeof(PixelRGB)));
 
-  /* Neigboring pixels have difference 1
+  /* Process the first (horizontal) pair of pixels created by 
+   * init_test_pixels_1.
    */
-  histogram_process_pixel_pair(&h, init_test_pixels_1(pixels), index, next);
-  assert(histogram_get(&h, bin_index, index) == 1);
+  histogram_process_pixel_pair(&h, init_test_pixels_1(pixels), 0, 1);
+  /* Neigboring pixels have difference 1.
+   */
+  assert(histogram_get(&h, 1, 0) == 1);
 }
 
-void test_histogram_thread_x(){
+static void test_histogram_thread_x(){
   histogram h={0};
-  PixelRGB[64] pixels={0};
-  histogram_thread_arg arg = {&h, &init_test_pixels_0(&pixels)};
+  PixelRGB pixels[64]={0};
+  histogram_thread_arg arg = {&h, init_test_pixels_0(pixels)};
   histogram_thread_x(&arg);
-  assert(1);
-  return 1;
+  /* All 64 horizontal neighbor pairs have difference 0;
+   */
+  assert(histogram_get(&h, 0, 0) == 64);
 }
 
-void test_histogram_thread_y(){
+static void test_histogram_thread_y(){
   histogram h={0};
-  PixelRGB[64] pixels={0};
-  histogram_thread_arg arg = {&h, &init_test_pixels_0(&pixels)}
+  PixelRGB pixels[64]={0};
+  histogram_thread_arg arg = {&h, init_test_pixels_0(pixels)};
   histogram_thread_y(&arg);
-  assert(1);
+  /* All 64 vertical neighbor pairs have difference 0;
+   */
+  assert(histogram_get(&h, 0, 0) == 64);
 }
 
-void test_histogram(){
+static void test_histogram(){
   test_histogram_insert();
   test_histogram_remove();
   test_histogram_median();
