2ab6e8a167b02a7a098afb4f57e026a5ae97e449
angstyloop <angstyloop@gmail.com>
2021-08-19 18:02:12 -0700

    got generate_nonduplicates.c working finally. can be simplified, but moving onto ROC stuff. I can do some more simplification when I hook up the tests to a Makefile.

--- generate_nonduplicates.c
+++ generate_nonduplicates.c
@@ -1,49 +1,49 @@
 /*  generate_nonduplicates.c
  *
  *  Copy all the files from <SOURCE_DIR> to <TARGET_DIR>, making two copies of
- *  each file, with suffixes "_a" and "_b". The basename of the second copy
- *  will be changed to match the previous element (wrapping at the end), so
- *  that a/b pairs with identical basenames are nonduplicates (assuming the
- *  source directory contains only unique files). 
+ *  each file, with suffixes "_a" and "_b" (before prefix). The basename of the 
+ *  second copy will be changed to match the previous element (wrapping at the
+ *  end), so that a/b pairs with identical basenames are nonduplicates
+ *  (assuming the source directory contains only unique files).
  *
- *  Compile
+ *  COMPILE
  * 
  *  gcc generate_nonduplicates.c -o generate-nonduplicates -luuid
  *
  *
- *  Usage
+ *  USAGE
  *
  *  ./generate_nonduplicates <SOURCE_DIR> <TARGET_DIR>
  *
  *
- *  Compile Test
+ *  COMPILE TEST
  *
  *  gcc generate_nonduplicates.c -o test-generate-nonduplicates -luuid
  *    -DTEST_GENERATE_NONDUPLICATES
  * 
  * 
- *  Test
+ *  TEST
  * 
  *  ./test-generate-nonduplicates
  * 
  */
 
-#ifndef GUARD_STDLIB
+#ifndef STDLIB_H
 #  define GUARD_STDLIB
 #  include <stdlib.h>
 #endif
 
-#ifndef GUARD_STDIO
+#ifndef STDIO_H
 #  define GUARD_STDIO
 #  include <stdio.h>
 #endif
 
-#ifndef GUARD_UUID
+#ifndef UUID_H
 #  define GUARD_UUID
 #  include <uuid/uuid.h>
 #endif
 
-#ifndef GUARD_UNISTD
+#ifndef UNISTD_H
 #  define GUARD_UNISTD
 #  include <unistd.h>
 #endif
@@ -74,16 +74,18 @@
 typedef struct file_info file_info;
 struct file_info {
   int fd;
-  stat stat;
+  struct stat stat;
 };
 
-file_info* file_info_init(file_info* file_info, char path[SZ_PATH]){
-  return open_and_stat(&file_info, path) ? file_info : 0;
+int open_and_stat(file_info* f_info, char* path);
+
+file_info* file_info_init(file_info* f_info, char path[SZ_PATH]){
+  return open_and_stat(f_info, path) ? 0 : f_info;
 }
 
-file_info* file_info_zero(file_info* file_info){
-  memset(file_info, 0, sizeof(file_info));
-  return file_info;
+file_info* file_info_zero(file_info* f_info){
+  memset(f_info, 0, sizeof(file_info));
+  return f_info;
 }
 
 void copy_file_from_to(char* source, char* target){
@@ -95,32 +97,31 @@ void copy_file_from_to(char* source, char* target){
 
 typedef struct dirent_info dirent_info;
 struct dirent_info {
-  DIR* dirp,
-  struct dirent* direntp,
+  DIR* dirp;
+  struct dirent* direntp;
 };
 
 void open_dir(DIR** dirp, char* dir_name);
 
-dirent_info* dirent_info_init(dirent_info* dirent_info, char* dir_name){
-  open_dir(&dirent_info->dirp, dir_name);
-  return dirent_info;
+dirent_info* dirent_info_init(dirent_info* d_info, char* dir_name){
+  open_dir(&d_info->dirp, dir_name);
+  return d_info;
 }
 
-dirent_info* dirent_info_zero(dirent_info* d){
-  memset(d, 0, sizeof(dirent_info));
-  return d;
+dirent_info* dirent_info_zero(dirent_info* d_info){
+  memset(d_info, 0, sizeof(dirent_info));
+  return d_info;
 }
 
-int open_and_stat(file_info* infop, char* path) {
-  if(0>(infop->fd = open(path, O_RDONLY)) || fstat(infop->fd, statp)){
+int open_and_stat(file_info* f_info, char* path) {
+  if(0>(f_info->fd = open(path, O_RDONLY)) 
+    || fstat(f_info->fd, &f_info->stat)){
     fprintf(stderr, "Failed to open and stat file %s\n", path);
     return 1;
-  } else {
-    return 0;
-  }
+  } else return 0;
 }
 
-typedef struct file_path; 
+typedef struct file_path file_path; 
 struct file_path {
   char path[SZ_PATH];
   char dir[SZ_PATH];
@@ -129,56 +130,75 @@ struct file_path {
   char name_wext[2*SZ_NAME];
 };
 
-file_path* file_path_init(file_path* p, char* dir, char* name_wext){
-  p->dir = dir;
-  p->name_wext = name_wext;
-  join_dir_to_name(p->path, p->dir, p->name_wext);
-  split(p->name, p->ext, p->name_wext);
-  return p;
+file_path* file_path_init(
+  file_path* f_path,
+  char dir[SZ_PATH],
+  char name_wext[2*SZ_NAME])
+{
+  strncpy(f_path->dir, dir, SZ_PATH);
+  strncpy(f_path->name_wext, name_wext, 2*SZ_NAME);
+  join_dir_to_name(f_path->path, f_path->dir, f_path->name_wext);
+  split(f_path->name, f_path->ext, f_path->path);
+  return f_path;
 }
 
-file_path* file_path_zero(file_path* p){
-  memset(p, 0, sizeof(file_path));
-  return p;
+file_path* file_path_zero(file_path* f_info){
+  memset(f_info, 0, sizeof(file_path));
+  return f_info;
 }
 
-dirent_info* dirent_info_next_entry(dirent_info* dirent_infop){
-  if(!(dirent_infop->direntp = readdir(dirent_infop->dirp))){
-    closedir(dirent_infop->dirp);
-    fprintf(stderr, "Directory empty %s.", source_dir);
+dirent_info* dirent_info_next_entry(dirent_info* d_infop){
+  if(!(d_infop->direntp = readdir(d_infop->dirp))){
+    fprintf(stderr, "All dirents have been read.");
     exit(EXIT_FAILURE);
   }
-  return dirent_infop;
+  return d_infop;
 }
 
 int next_regular_file(
-  file_info* file_infop,
-  dirent_info* dirent_infop, 
-  file_path* file_pathp)
+  file_info* f_infop,
+  dirent_info* d_infop, 
+  file_path* f_pathp,
+  int offset)
 {
-  // Skip non-regular files.
-  while(!S_ISREG(file_infop->stat.st_mode)){
+  // Return 0 if offset is negative, to avoid an infinite loop.
+  if(0 > offset) return 0;
+
+  // Skip the first @offset dirents, and then skip all non-regular files. If
+  // the offset is 0, and the given dirent_info has a dirent that is already
+  // a regular file, does nothing.
+  struct dirent* direntp=0;
+  for(;;){
+
+    if(offset > 0){
+      --offset;
+    } else if(S_ISREG(f_infop->stat.st_mode)){
+      break;
+    }
+
     // Free the previous directory entry's file descriptor.
-    close(file_infop->fd);
+    close(f_infop->fd);
 
     // Return 0 when there are no more directory entries (or an error).
-    if(!(*dirent_infop->direntp = readdir(dirent_infop->dirp))) 
+    if(!(direntp = readdir(d_infop->dirp)))
       return 0;
+    d_infop->direntp = direntp;
 
     // Form input path to the next file.
-    join_dir_to_name(file_pathp->path, file_pathp->dir, file_pathp->name);
+    file_path_init(f_pathp, f_pathp->dir, d_infop->direntp->d_name);
 
-    // Open and stat the next file. Return 0 on failure to open/stat.
-    if(open_and_stat(file_infop, file_path->path))
+    // Open and stat the next file. Return 0 on failure to open/stat. Save 
+    // the fd and stat in the object pointed to by f_infop.
+    if(!file_info_init(f_infop, f_pathp->path)) 
       return 0;
   }
 
-  // Return 1 on successful iteration.
+  // Return 1 by default
   return 1;
 }
 
-/* Open directory named dir_name. Save the DIR pointer at the address pointed
- * to by dirp.
+/* Open directory named dir_name. Save the DIR pointer at the address 
+ * pointed to by dirp.
  */
 void open_dir(DIR** dirp, char* dir_name){
   if(!(*dirp = opendir(dir_name))){
@@ -188,7 +208,7 @@ void open_dir(DIR** dirp, char* dir_name){
 }
 
 /* Join @name to extension @ext, separated by separator @sep. Write the result
- * to name_wext.
+ * to output buffer @name_wext.
  */
 char* join_name_to_ext(
   char name_wext[3*SZ_NAME],
@@ -197,20 +217,20 @@ char* join_name_to_ext(
   char sep[SZ_NAME])
 {
   snprintf(name_wext, 3*SZ_NAME, "%s%s%s", name, sep, ext);
+  return name_wext;
 }
 
-// Defined in file scope, so the at_exit handler function close_files_and_dirs
-// can clean them up if they aren't zeroed.
-file_info prev_file_info={0}, file_info={0};
-dirent_info dirent_info={0};
+// Defined in file scope, so that atexit handler can clean up.
+file_info prev_f_info={0}, f_info={0}, first_f_info={0}; 
+dirent_info d_info={0};
 
-// An at_exit handler responsible for closing any open file descriptors or DIR*.
+// An atexit handler responsible for closing any open file descriptors or DIR*.
 // Assumes the fd and DIR* objects are non-zero/non-null if and only if they are
 // open, and need to be closed.
 static void close_files_and_dirs(){
-  if(file_info.fd) close(file_info.fd);
-  if(prev_file_info.fd) close(prev_file_info.fd)
-  if(dirent_info.dirp) closedir(dirent_info.dirp); 
+  if(f_info.fd) close(f_info.fd);
+  if(prev_f_info.fd) close(prev_f_info.fd);
+  if(d_info.dirp) closedir(d_info.dirp); 
 };
 
 /* Make two copies of each regular file in @source_dir. Put the copies in
@@ -232,64 +252,70 @@ static void generate_nonduplicates(
 
   // file_path objects used as the previous and current indices in the loop
   // over dirents.
-  file_path file_path={0}, prev_file_path={0}; 
+  file_path f_path={0}, prev_f_path={0}, first_f_path={0}; 
 
   // Schedule cleanup for any open file descriptors or DIR pointers at exit.
-  at_exit(close_files_and_dirs);
+  atexit(close_files_and_dirs);
 
   // Init the dirent_info we'll use to walk through the dirent objects returned
   // by readdir in dirent_info_next_entry. 
-  dirent_info_init(&dirent_info, source_dir);
+  dirent_info_init(&d_info, source_dir);
 
-  // Load the first dirent and its path, and open it.
-  dirent_info_next_entry(&dirent_info);
-  file_path_init(&prev_file_path, source_dir, dirent_info.direntp->d_name);
-  if(!file_info_init(&prev_file_info, prev_file_path.path)){
-    fprintf(stderr, "Failed to open first file %s.\n", prev_file_info.path);
+  // Load the first dirent and its path, and open it. Save the file descriptor
+  // and stat in a file_info object. Finally advance previous to the 
+  // regular file. Save that first file's so we can wrap around at the end.
+  dirent_info_next_entry(&d_info);
+  file_path_init(&prev_f_path, source_dir, d_info.direntp->d_name);
+  if(!file_info_init(&prev_f_info, prev_f_path.path)){
+    fprintf(stderr, "Failed to open first file %s.\n", prev_f_path.path);
     exit(EXIT_FAILURE);
   } 
 
-  // Save the first dirent's path for later. It's used with the last dirent to 
-  // form the last non-duplicate pair. 
-  strncpy(first_in_path, SZ_PATH, prev_file_path.path);
+  next_regular_file(&prev_f_info, &d_info, &prev_f_path, 0);
+  first_f_info = prev_f_info;
+  first_f_path = prev_f_path;
 
-  // Load the next dirent and its path, and open it.
-  dirent_info_next_entry(&dirent_info);
-  file_path_init(&file_path, source_dir, dirent_info.direntp->d_name);
-  if(!file_info_init(&file_info, file_path.path)){
-    fprintf(stderr, "Failed to open second file %s.\n", file_info.path);
+  dirent_info_next_entry(&d_info);
+
+  file_path_init(&f_path, source_dir, d_info.direntp->d_name);
+
+  if(!file_info_init(&f_info, f_path.path)){
+    fprintf(stderr, "Failed to open second file %s.\n", f_path.path);
     exit(EXIT_FAILURE);
   } 
 
-  // With the previous and current indices of our "iterator" initialized, we 
-  // can walk through the remaining dirents with a loop.
-  while(next_regular_file(&file_info, &dirent_info, &file_path)){
+  next_regular_file(&f_info, &d_info, &f_path, 0);
 
-    // Create output file b using the previous file's data and a modified 
-    // version of the current file's name.
-    join_name_to_ext(name_wext_b, file_path.name, file_path.ext, "_b.");
+  join_name_to_ext(name_wext_b, f_path.name, f_path.ext, "_b.");
+  join_dir_to_name(out_path_b, target_dir, name_wext_b);
+  copy_file_from_to(prev_f_path.path, out_path_b);
+
+  join_name_to_ext(name_wext_a, f_path.name, f_path.ext, "_a.");
+  join_dir_to_name(out_path_a, target_dir, name_wext_a);
+  copy_file_from_to(f_path.path, out_path_a);
+
+  while(next_regular_file(&f_info, &d_info, &f_path, 1)){
+    join_name_to_ext(name_wext_b, f_path.name, f_path.ext, "_b.");
     join_dir_to_name(out_path_b, target_dir, name_wext_b);
-    copy_file_from_to(prev_file_info.path, out_path_b);
+    copy_file_from_to(prev_f_path.path, out_path_b);
 
-    // Create output file a using the current file's data and a modified 
-    // version of its name.
-    join_name_to_ext(name_wext_a, file_path.name, file_path.ext, "_a.");
+    join_name_to_ext(name_wext_a, f_path.name, f_path.ext, "_a.");
     join_dir_to_name(out_path_a, target_dir, name_wext_a);
-    copy_file_from_to(file_path.path, out_path_a);
+    copy_file_from_to(f_path.path, out_path_a);
 
-    // Copy current data to previous data, so the next iteration of the loop 
-    // will see the right previous and current data. Free the old previous
-    // file's descriptor.
-    close(prev_file_info.fd);
-    prev_file_info = file_info;
-    prev_file_path = file_path;
+    close(prev_f_info.fd);
+    prev_f_info = f_info;
+    prev_f_path = f_path;
   }
 
-  // Treat the last pair separately. Form the last pair by wrapping around 
-  // (so the first file is the one used for the output file base name and 
-  // data). 
-  copy_file_from_to(prev_file_path.path, first_out_path_b);
-  copy_file_from_to(first_in_path, first_out_path_a);
+  // last
+  join_name_to_ext(name_wext_a, first_f_path.name, first_f_path.ext, "_a.");
+  join_dir_to_name(out_path_a, target_dir, name_wext_a);
+  copy_file_from_to(first_f_path.path, out_path_a);
+
+  join_name_to_ext(name_wext_b, first_f_path.name, first_f_path.ext, "_b.");
+  join_dir_to_name(out_path_b, target_dir, name_wext_b);
+  copy_file_from_to(f_path.path, out_path_b);
 }
 
 #ifdef TEST_GENERATE_NONDUPLICATES
@@ -333,12 +359,15 @@ int main(){
     exit(EXIT_FAILURE);
   } 
   generate_nonduplicates(source_dir_name, target_dir_name);
-  char exp_path_1_a[SZ_PATH], exp_path_1_b[SZ_PATH], 
-    exp_path_2_a[SZ_PATH], exp_path_2_b[SZ_PATH];
+
+  // Form the expected paths for the output files, and test they are correct.
+  char exp_path_1_a[SZ_PATH]={0}, exp_path_1_b[SZ_PATH]={0}, 
+    exp_path_2_a[SZ_PATH]={0}, exp_path_2_b[SZ_PATH]={0};
   join_dir_to_name(exp_path_1_a, target_dir_name, "test-file-1_a.txt");
   join_dir_to_name(exp_path_1_b, target_dir_name, "test-file-1_b.txt");
   join_dir_to_name(exp_path_2_a, target_dir_name, "test-file-2_a.txt");
   join_dir_to_name(exp_path_2_b, target_dir_name, "test-file-2_b.txt");
+
   assert((f1 = fopen(exp_path_1_a, "r")) && (f2 = fopen(exp_path_1_b, "r")));
   if(f1){
     fclose(f1);
@@ -348,6 +377,7 @@ int main(){
     fclose(f2);
     f2=0;
   }
+
   assert((f1 = fopen(exp_path_2_a, "r")) && (f2 = fopen(exp_path_2_b, "r")));
   if(f1){
     fclose(f1);
@@ -357,9 +387,12 @@ int main(){
     fclose(f2);
     f2=0;
   }
-  if(remove(exp_path_1_a) || remove(exp_path_1_b) || remove(exp_path_2_a)
+
+  // Clean up test files and dirs.
+  if(  remove(exp_path_1_a) || remove(exp_path_1_b)    || remove(exp_path_2_a)
     || remove(exp_path_2_b) || remove(target_dir_name) || remove(test_path_1)
-    || remove(test_path_2) || remove(source_dir_name)){
+    || remove(test_path_2)  || remove(source_dir_name)){
+
     fprintf(stderr, "Failed to delete temporary files and directories.");
     exit(EXIT_FAILURE);
   } 
@@ -367,7 +400,7 @@ int main(){
 }
 #else
 int main(int argc, char* argv[]){
-  if(argc!=3 || *argv[1] || *argv[2]){
+  if(argc!=3 || !*argv[1] || !*argv[2]){
     fprintf(stderr, "Usage: %s <SOURCE_DIR> <TARGET_DIR>", argv[0]);
     exit(EXIT_FAILURE);
   } 
