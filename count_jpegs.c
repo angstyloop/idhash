e798028e46c890794fe7bd9e527881f2e1b8f5a6
angstyloop <angstyloop@gmail.com>
2021-09-07 15:01:29 -0700

    created and tested count_jpegs.c, renamed temp_dir_and_files_example.c to path.c

--- count_jpegs.c
+++ count_jpegs.c
@@ -1,8 +1,8 @@
-/*
+/******************************************************************************* 
 gcc -g -Wall count_jpegs.c -o test-count-jpegs -DTEST_COUNT_JPEGS
 
 Count the jpegs in a directory.
-*/
+*******************************************************************************/
 
 #ifndef STDLIB_H
 #  define STDLIB_H
@@ -14,21 +14,172 @@ Count the jpegs in a directory.
 #  include <stdio.h>
 #endif
 
-void count_jpegs(int* n, DIR* pdir){
-  // iterate through each regular file in directory
-  //   get name of file
-  //   count file names ending in ".jpeg", ".jpg", ".JPG", or ".JPEG".
+#ifndef TYPES_H
+#  define TYPES_H
+#  include <sys/types.h>
+#endif
+
+#ifndef DIRENT_H
+#  define DIRENT_H
+#  include <dirent.h>
+#endif
+
+#ifndef STAT_H
+#  define STAT_H
+#  include <sys/stat.h>
+#endif
+
+#ifndef UNISTD_H
+#  define UNISTD_H
+#  include <unistd.h>
+#endif
+
+#ifndef FCNTL_H
+#  define FCNTL_H
+#  include <fcntl.h>
+#endif
+
+#ifndef ASSERT_H
+#  define ASSERT_H
+#  include <assert.h>
+#endif
+
+#ifndef PATH_H
+#  define PATH_H
+#  include "path.c"
+#endif
+
+// return 1 if @substr matches the end of @str (and is no bigger than @str),
+// else return 0.
+int ends_with(char* s, char* ss){
+  size_t ns = strlen(s), nss = strlen(ss);
+  return nss > ns ? 0 : !strncmp(s + ns - nss, ss, nss);
 }
 
-int main(){
-  //make tmp dir
-  //count jpegs in tmp dir
-  //assert count is zero
-  //make a jpg file in tmp dir for each jpeg pattern (4)
-  //count jpegs in tmp dir
-  //assert count is 4
-  //remove files (!)
-  //remove tmpdir (!)
-  //
-  //(!) not required
+// 0-terminated list of accepted jpeg extensions
+static char* jpeg_exts[] = {"jpg", "JPG", "jpeg", "JPEG", 0};
+
+// return a newly allocated string just big enough for "${s1}${s2}".
+// the returned string must be freed.
+char* concat(char* s1, char* s2){
+  size_t n = strlen(s1) + strlen(s2) + 1/*nullbyte*/;
+  char* s = calloc(n, 1);
+  snprintf(s, n, "%s%s", s1, s2);
+  return s;
 }
+
+// return 1 if @path has an extension from @jpeg_extensions
+int is_jpeg(char* path){
+  for(char** pext=jpeg_exts; *pext; ++pext){
+    char* dot_ext = concat(".", *pext);
+    if(ends_with(path, dot_ext)) return 1;
+    free(dot_ext);
+  }
+  return 0;
+}
+
+// count file names with extension from @jpeg_exts
+void count_jpegs(unsigned* count, char* dname){
+  // iterate through each regular file in directory, counting the paths with 
+  // a jpeg extension from @jpeg_exts
+  DIR* pdir = opendir(dname);
+  int fd=-1;
+  struct dirent* pdirent=0;
+  struct stat st={0};
+  while((pdirent = readdir(pdir))){
+    path* ppath = path_new(dname, pdirent->d_name, "");
+    char* fullpath = path_full(ppath);
+
+    // open and stat file
+    if(-1 == (fd = open(fullpath, O_RDONLY)) || fstat(fd, &st)){
+      fprintf(stderr, "Failed to open/stat file %s.\n", fullpath);
+      exit(EXIT_FAILURE);
+    }
+
+    // is it a regular file? does it have a jpeg extension?
+    if(S_ISREG(st.st_mode) && is_jpeg(fullpath))
+      ++(*count);
+
+    //clean up
+    free(fullpath);
+    path_free(ppath);
+    close(fd);
+  }
+  closedir(pdir);
+}
+
+#ifdef TEST_COUNT_JPEGS
+int main(){
+  char dname[] =  "/tmp/test_count_jpegs_XXXXXX";
+  //make tempdir
+  if(!(mkdtemp(dname))){
+    fprintf(stderr, "Unable to make tempdir %s.\n", dname);
+    exit(EXIT_FAILURE);
+  } 
+
+  //count jpegs in tmp dir
+  unsigned count=0;
+  count_jpegs(&count, dname);
+
+  //assert count is zero
+  assert(!count);
+
+  //loop over extensions. make a tempfile for each (4 total).
+  for(char** pext = jpeg_exts; *pext; ++pext){
+    //make tempfile path object
+    path* ppath = path_new(dname, "test_file_XXXXXX", *pext);
+    //create full path string
+    char* pfull = path_full(ppath);
+    //make tempfile
+    if(0>mkstemps(pfull, path_has_ext(ppath) ? 
+      1/*dot*/ + strlen(ppath->ext) : 0))
+    {
+      fprintf(stderr, "Unable to make tempfile %s.\n", pfull);
+      free(pfull);
+      path_free(ppath);
+      exit(EXIT_FAILURE);
+    } 
+    free(pfull);
+    path_free(ppath);
+  }
+
+  //count jpegs in tmp dir
+  count=0;
+  count_jpegs(&count, dname);
+
+  //assert count is 4
+  assert(count == 4);
+
+  //remove all regular files in dname
+  int fd=0;
+  struct dirent* pdirent=0;
+  struct stat st={0};
+  DIR* pdir = opendir(dname);
+  while((pdirent = readdir(pdir))){
+    path* ppath = path_new(dname, pdirent->d_name, "");
+    char* fullpath = path_full(ppath);
+    if(-1 == (fd = open(fullpath, O_RDONLY)) || fstat(fd, &st)){
+      fprintf(stderr, "Unable to open/stat tempfile %s.\n", fullpath);
+      exit(EXIT_FAILURE);
+    } 
+    if(S_ISREG(st.st_mode) && remove(fullpath)){
+      fprintf(stderr, "Error removing tempfile %s.\n", fullpath);
+      free(fullpath);
+      free(ppath);
+      exit(EXIT_FAILURE);
+    } 
+    free(fullpath);
+    free(ppath);
+  }
+  closedir(pdir);
+
+  //remove tmpdir
+  if(remove(dname)){
+    fprintf(stderr, "Error removing tempdir %s.\n", dname);
+    exit(EXIT_FAILURE);
+  } 
+
+  puts("OK");
+  return EXIT_SUCCESS;
+}
+#endif
--- path.c
+++ path.c
@@ -1,5 +1,5 @@
 /*
-gcc temp_dir_and_files_example.c -o temp-dir-and-files-example -g -Wall
+gcc path.c -o test-path -DTEST_PATH -g -Wall
 */
 
 #include <stdlib.h>
@@ -12,47 +12,106 @@ struct path {
   char* dir;
   char* name;
   char* ext;
+  char* full;
 };
 
+static char* path_full(path* ppath);
+
 path* path_create(){
-  return calloc(1, sizeof(path));
+  path* ppath = calloc(1, sizeof(path));
+  ppath->dir = calloc(1, 1);
+  ppath->name = calloc(1, 1);
+  ppath->ext = calloc(1, 1);
+  ppath->full = calloc(1, 1);
+  return ppath;
 }
+
+// Return true iff dir is neither 0 nor "".
+int path_has_dir(path* ppath){
+  return *ppath->dir;
+}
+
+// Return true iff name is neither 0 nor "".
+int path_has_name(path* ppath){
+  return *ppath->name;
+}
+
+// Return true iff ext is neither 0 nor "".
+int path_has_ext(path* ppath){
+  return *ppath->ext;
+}
+
+// Return true iff the path object is empty.
+int path_empty(path* ppath){
+  return !( path_has_dir(ppath) 
+    || path_has_name(ppath) 
+    || path_has_ext(ppath) );
+}
+
 path* path_init(
   path* ppath, 
-  char dir[static 1],
-  char name[static 1],
-  char ext[static 1])
+  char* dir,
+  char* name,
+  char* ext)
 {
   size_t n=0; 
 
-  n = strlen(dir);
-  ppath->dir = calloc(n+1, 1);
-  strncpy(ppath->dir, dir, n);
+  if(*name){
+    n = strlen(name);
+    free(ppath->name);
+    ppath->name = calloc(n+1, 1);
+    strncpy(ppath->name, name, n);
 
-  n = strlen(name);
-  ppath->name = calloc(n+1, 1);
-  strncpy(ppath->name, name, n);
+    if(!strcmp("..", ppath->name)){
+      free(ppath->full);
+      ppath->full = calloc(2, 1);
+      ppath->full[0] = ppath->full[1] = '.';
+      return ppath;
+    }
 
-  n = strlen(ext);
-  ppath->ext = calloc(n+1, 1);
-  strncpy(ppath->ext, ext, n);
+    if(!strcmp(".", ppath->name)){
+      free(ppath->full);
+      ppath->full = calloc(1, 1);
+      ppath->full[0] = '.';
+      return ppath;
+    }
+  }
 
+  if(*dir){
+    n = strlen(dir);
+    free(ppath->dir);
+    ppath->dir = calloc(n+1, 1);
+    strncpy(ppath->dir, dir, n);
+  }
+
+  if(*ext){
+    n = strlen(ext);
+    free(ppath->ext);
+    ppath->ext = calloc(n+1, 1);
+    strncpy(ppath->ext, ext, n);
+  }
+
+  free(ppath->full);
+  ppath->full = path_full(ppath);
   return ppath;
 }
+
 path* path_new(
-  char dir[static 1],
-  char name[static 1],
-  char ext[static 1])
+  char* dir,
+  char* name,
+  char* ext)
 {
   return path_init(path_create(), dir, name, ext);
 }
+
 path* path_free(path* ppath){
-  if(ppath->dir) free(ppath->dir);
-  if(ppath->name) free(ppath->name);
-  if(ppath->ext) free(ppath->ext);
-  free(ppath);
+  free(ppath->dir);
+  free(ppath->name);
+  free(ppath->ext);
+  free(ppath->full);
   return ppath;
 }
+
 path* path_print(path* ppath){
   printf("dir: %s%*cname: %s%*cext: %s%*c\n",
     ppath->dir, 4, ' ', 
@@ -60,41 +119,41 @@ path* path_print(path* ppath){
     ppath->ext, 4, ' ');
   return ppath;
 }
-char* path_full(path* ppath){
-  size_t n = strlen(ppath->dir) + 1/*slash*/ + strlen(ppath->name) + 1/*dot*/ 
-    + strlen(ppath->ext) + 1/*nullbyte*/;
+
+unsigned path_ext_len(path* ppath){
+  return path_has_ext(ppath) ? strlen(ppath->ext) : 0;
+}
+
+static char* path_full(path* ppath){
+  int wext = path_has_ext(ppath);
+  int wdir = path_has_dir(ppath);
+
+  size_t n = strlen(ppath->dir) + (wdir ? 1/*slash*/ : 0) 
+    + strlen(ppath->name)
+    + (wext ? 1/*dot*/ : 0)  + strlen(ppath->ext)
+    + 1/*nullbyte*/;
+
   char* fullpath = calloc(n, 1);
-  snprintf(fullpath, n, "%s/%s.%s", ppath->dir, ppath->name, ppath->ext);
+
+  snprintf(fullpath, n, "%s%s%s%s%s", 
+    ppath->dir,
+    wdir ? "/" : "", 
+    ppath->name, 
+    wext ? "." : "",  
+    ppath->ext);
+
   return fullpath;
 }
-char* path_full_no_ext(path* ppath){
-  size_t n = strlen(ppath->dir + 1/*slash*/ + strlen(ppath->name) +
-    + 1/*nullbyte*/);
-  char* p = calloc(n, 1);
-  snprintf(p, n, "%s/%s", ppath->dir, ppath->name);
-  return p;
-}
-char* path_full_no_ext_w_dot(path* ppath){
-  size_t n = strlen(ppath->dir + 1/*slash*/ + strlen(ppath->name) + 1/*dot*/ 
-    + 1/*nullbyte*/);
-  char* p = calloc(n, 1);
-  snprintf(p, n, "%s/%s.", ppath->dir, ppath->name);
-  return p;
-}
-char* path_ext_w_dot(path* ppath){
-  size_t n = 1/*dot*/ + strlen(ppath->ext);
-  char* p = calloc(n, 1);
-  snprintf(p, n, ".%s", ppath->ext);
-  return p;
-}
-path* path_set_ext(path* ppath, char ext[static 1]){
-  if(ppath->ext) free(ppath->ext);
+
+path* path_set_ext(path* ppath, char* ext){
+  free(ppath->ext);
   size_t n = strlen(ext) + 1/*nullbyte*/;
   ppath->ext = calloc(n, 1);
   strncpy(ppath->ext, ext, n-1);
   return ppath;
 }
 
+#ifdef TEST_PATH
 int main(){
   //use "jpg" as the first extension
   path* ppath = path_new("/tmp/tempdir.XXXXXX", "tempfile.XXXXXX", "jpg");
@@ -113,7 +172,7 @@ int main(){
   char* p_full = path_full(ppath);
 
   //create first temporary file
-  if(0>(mkstemps(p_full, strlen(ppath->ext) + 1))){
+  if(0>(mkstemps(p_full, strlen(ppath->ext) + 1/*dot*/))){
     fprintf(stderr, "Unable to create temporary file %s\n", p_full);
     path_free(ppath);
     free(p_full);
@@ -139,7 +198,7 @@ int main(){
   p_full = path_full(ppath);
 
   //create second temporary file
-  if(0>(mkstemps(p_full, strlen(ppath->ext) + 1))){
+  if(0>mkstemps(p_full, strlen(ppath->ext) + 1)){
     fprintf(stderr, "Unable to create temporary file %s\n", p_full);
     free(p_full);
     path_free(ppath);
@@ -170,3 +229,4 @@ int main(){
   //all done!
   return EXIT_SUCCESS;
 }
+#endif
